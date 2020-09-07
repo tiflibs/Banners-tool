c8c8ac1d5f20fafcb8abeba2ce922b3b0aa8fb93
tiflibs <70098265+tiflibs@users.noreply.github.com>
2020-09-07 22:09:02 +0530

    Update and rename banner.sh to Installation.sh

---
+++ Installation.sh
@@ -0,0 +1,13 @@
+cd
+gem install lolcat
+pkg install figlet 
+pkg install toilet 
+pkg install cowsay 
+cd unique-bot
+mv agent.cow /$HOME
+mv eyes.cow /$HOME
+cd
+mv agent.cow ../usr/share/cows
+mv eyes.cow ../usr/share/cows
+cd
+
